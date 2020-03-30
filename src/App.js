71a69e90e9f72a170f6b492b265e31ba760e1fcf
Patryk Kaczor <51462170+Proceder37@users.noreply.github.com>
2020-03-30 14:38:39 +0200

    Registration window

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from './components/navbar/Navbar'
 import Body from './views/test-page/Body'
+import Reg from './views/Reg';
 import React from 'react';
 import './App.scss';
+
 
 function App() {
   return (
     <div className="bg-light App">
       <Navbar />
-      <Body />
+      <Reg />
     </div>
   );
 }
