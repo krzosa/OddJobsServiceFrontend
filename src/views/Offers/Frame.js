ae9a2a1ff4c4b887efd65ca67afa8077e6b1dfc6
Eryk1997 <erykmati@o2.pl>
2020-04-05 13:43:47 +0200

    add require

--- src/views/Offers/Frame.js
+++ src/views/Offers/Frame.js
@@ -1,35 +1,40 @@
 import React, { Component } from "react";
-import Work from './images/work.jpg';
+import IMG from './images/allegro.jpg'
 
 class Frame extends Component {
   render() {
+    var data = require("./Data/data.json");
+    //console.log(data["info"][0]["jobName"]);
     return (
       <div className="container mt-2 ">
-        <div className="row">
-          <div className="card mb-3 col-12" style={{ width: "540px" }}>
-            <div className="row no-gutters">
-              <div className="col-md-4 mt-2 mb-2">
-                <img src={Work} className="card-img" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Nazwa pracy</a>
-                  </h4>
-                  <h5 className="card-title">
-                    <a href="#">Nazwa firmy</a>
-                  </h5>
-                  <p className="card-text">Miasto</p>
-
-                  <p className="card-text">Opis</p>
-                  <p className="card-text">
-                    <small className="text-muted">Data publikacji</small>
-                  </p>
+        {data.info.map((item) => (
+          <div className="row">
+            <div className="card mb-3 col-12" style={{ width: "540px" }}>
+              <div className="row no-gutters">
+                <div className="col-md-4 mt-2 mb-2">
+                  <img src={item.img} className="card-img" alt={item.jobName} />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h4 className="card-title">
+                      <a href="#">{item.jobName}</a>
+                    </h4>
+                    <h5 className="card-title">
+                      <a href="#">{item.companyName}</a>
+                    </h5>
+                    <p className="card-text">{item.city}</p>
+                    <p className="card-text">{item.description}</p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        {item.dateOfPublication}
+                      </small>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     );
   }
