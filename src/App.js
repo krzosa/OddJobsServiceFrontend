d4a83b60c05f6c669bd28f172fa499e72599f755
Eryk1997 <erykmati@o2.pl>
2020-04-11 13:12:40 +0200

    add path details from Details

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ import Registration from './views/Registration';
 import Offers from './views/Offers/Offers';
 import Body from './views/test-page/Body'
 import About from './views/about/About';
+import Details from './views/Details/Details';
 
 function App() {
   return (
     <div className="bg-light App">
       <Navbar />
-  
       <Switch>
         <Route exact path="/" component={Body} />
         <Route path="/praca" component={Offers} />
         <Route path="/rejestracja" component={Registration} />
         <Route path="/about" component={About} />
+        <Route path="/details" component={Details} />
       </Switch>
-
       <Footer />
     </div>
   );
