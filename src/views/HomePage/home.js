5a70317012183c0f032a0249a6c9f3015d1f018f
Sergio <alan.kazek@gmail.com>
2020-04-17 14:42:04 +0200

    Home Page v 0.02 - styled elements are centered, added css simple animation

--- src/views/HomePage/home.js
+++ src/views/HomePage/home.js
@@ -3,17 +3,18 @@ import "./style.css"
 class HomePage extends Component{
     render(){
         return(
-          <div className="container d-flex justify-content-center text-center text-light">
+        <div class="space">
+          <div className="container d-flex justify-content-center text-center text-light ">
             <div className="row">
               <div className="col-lg-6 ">
-                <div className="card mt-5 mr-5">
+                <div className="card mt-5 mr-5" style={{width: "18rem"}}>
                     <div className="card-body">
                       <h3 className="card-title font-weight-bold vertical">DAJ PRACĘ</h3>
                     </div>
                   </div>
               </div>
               <div className="col-lg-6">
-                <div className="card mt-5 ml-5" >
+                <div className="card mt-5 ml-5" style={{width: "18rem"}}>
                     <div className="card-body">
                       <h3 className="card-title font-weight-bold text-center vertical text-light">ZNAJDŹ PRACĘ</h3>
                     </div>
@@ -22,6 +23,7 @@ class HomePage extends Component{
               
             </div>
           </div> 
+        </div>
         );
     }
 }
