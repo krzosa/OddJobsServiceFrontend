e4419bf64934b76635f6ec73a2849e7cc256a119
Eryk1997 <erykmati@o2.pl>
2020-05-07 14:25:14 +0200

    create login

--- src/views/login/AlertFile.js
+++ src/views/login/AlertFile.js
@@ -1,14 +1,9 @@
-export function AlertFile(res){
-    if(res.data.success == "true"){
-        alert("Zalogowano");
-        document.getElementById("username").value = '';
-        document.getElementById("password").value = '';
-        document.getElementById("userNameId").innerHTML = '';
-        document.getElementById('passwordId').innerHTML = '';
-    }
-    else{
-        alert("Nie ma takiego konta lub złe hasło");
-
-    }
-    
+export function AlertFile(res) {
+  if (res.data.success == "true") {
+    alert("Zalogowano");
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("userNameId").innerHTML = "";
+    document.getElementById("passwordId").innerHTML = "";
+  } else alert("Nie ma takiego konta lub złe hasło");
 }
