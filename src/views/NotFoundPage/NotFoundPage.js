b2f7b3289d0353fe628010835d3138390b07b370
Adam <adas.machaj@gmail.com>
2020-05-17 13:31:56 +0200

    minor changes

--- src/views/NotFoundPage/NotFoundPage.js
+++ src/views/NotFoundPage/NotFoundPage.js
@@ -5,7 +5,9 @@ class NotFoundPage extends Component {
         return (
             <div className="container content">
                 <div className="text-center mt-5">
-                <h1>Nie ma takiej strony!</h1>
+                <h1>Oops! Page Not Found</h1>
+                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+			    <a href="#">Back to homepage</a>
                 </div>
             </div>
         )
