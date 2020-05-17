e8e278d74de520cdca77a9fa0fde3e2305bd7b58
Adam <adas.machaj@gmail.com>
2020-05-17 13:45:40 +0200

    Column changes

--- src/views/NotFoundPage/NotFoundPage.js
+++ src/views/NotFoundPage/NotFoundPage.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react'
 class NotFoundPage extends Component {
     render() {
         return (
+            
             <div className="container content">
                 <div className="text-center mt-5">
                 <h1>Oops! Page Not Found</h1>
-                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-			    {/* <a href="#">Back to homepage</a> */}
-                <a href="#" class="btn btn-primary btn-lg"> <span class="glyphicon glyphicon-arrow-left"></span> back to homepage </a>
+                    <div class="row justify-content-center">
+                        <div class="col-6">                        
+                            <p>Sorry but the page you are looking for does not exist, have been removed. Name changed or is temporarily unavailable</p>
+                            {/* <a href="#">Back to homepage</a> */}
+                            <a href="#" class="btn btn-primary btn-lg"> <span class="glyphicon glyphicon-arrow-left"></span> back to homepage </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
