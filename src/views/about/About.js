56e756b5ab0f836f1d1483a1dcacf0cbf91c91ce
Sergio <alan.kazek@gmail.com>
2020-05-17 19:25:00 +0200

    Improving about view

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -24,7 +24,6 @@ class About extends Component {
             <div className="row">
                 <div className="col">
                     <div className="card">
-                        <img className="card-img-top" src="" alt="Card cap"/>
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -34,7 +33,6 @@ class About extends Component {
                 </div>
                 <div className="col">
                 <div className="card">
-                        <img className="card-img-top" src="" alt="Card cap"/>
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -44,7 +42,6 @@ class About extends Component {
                 </div>
                 <div className="col">
                 <div className="card">
-                        <img className="card-img-top" src="" alt="Card cap"/>
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
