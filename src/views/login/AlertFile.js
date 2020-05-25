bf2792bf08b8ed9c84f56d421b5f6266f9ea1c95
Eryk1997 <erykmati@o2.pl>
2020-05-25 15:07:42 +0200

    set fields user and login status

--- src/views/login/AlertFile.js
+++ src/views/login/AlertFile.js
@@ -1,9 +1,18 @@
-export function AlertFile(res) {
+import auth from "../../Auth/Auth";
+import { GetUser } from "../../Auth/SetUser";
+
+export const AlertFile = (res, props, data) => {
   if (res.data.success == "true") {
-    alert("Zalogowano");
+    //alert("Zalogowano");
+    GetUser(data);
+
+    auth.login(() => {
+      props.history.push("/userpanel");
+    });
+
     document.getElementById("username").value = "";
     document.getElementById("password").value = "";
     document.getElementById("userNameId").innerHTML = "";
     document.getElementById("passwordId").innerHTML = "";
   } else alert("Nie ma takiego konta lub złe hasło");
-}
+};
