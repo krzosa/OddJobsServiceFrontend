d28bd78ff801d9424981bd096b02b865393ba6e9
Adam <adas.machaj@gmail.com>
2020-05-31 16:37:58 +0200

    Reload on click

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -33,8 +33,7 @@ class Navbar extends Component{
                        .catch(err => console.log(err));
                       auth.logout();
                       Cookies.remove('userName');
-                      location.reload();
-                    }}>Logout</button>
+                      }}>Logout</button>
                     </div>
                 </div>
               </div>
