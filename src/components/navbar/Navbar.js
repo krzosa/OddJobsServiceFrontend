f3df14ac959e6da8f1297402be98f2c5aa7b95a2
krzosa <krzosa.karol@gmail.com>
2020-06-05 21:58:04 +0200

    update conditional logout button

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,37 +5,46 @@ import auth from "../../Auth/Auth";
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-class Navbar extends Component{
-    render(){
-        return(
-            <nav className="navbar navbar-dark bg-primary">
-              <div className="container-fluid" id="navbarText">
-                <div className="navbar-header col-2">
-                <Link className="navbar-brand" to="">
-                <img className="col-12 pl-0" src={require('./logo_biale.png') } alt=""/>
-                </Link>
-                </div>
-                <div className="navbar-nav">
+class Navbar extends Component {
 
-                    <Link className="nav-link ml-5" to="praca">Oferty pracy</Link>
-                    <Link className="nav-link ml-5" to="about">O nas</Link>
-                    <Link className="nav-link ml-5" to="userpanel">Moje konto</Link>
-                    <div className="ml-3">
-                    <button onClick={() => {
-                       axios
-                       .get("http://149.156.146.249:60021/api/logout")
-                       .then(resp => console.log(resp))
-                       .catch(err => console.log(err));
-                      auth.logout();
-                      Cookies.remove('userName');
-                      window.location.reload();
-                    }}>Logout</button>
-                    </div>
-                </div>
-              </div>
-            </nav>
-        );
+  // Note(Krzosa): show logout button if authenticated, needs a refresh to work
+  logoutButton() {
+    if (auth.isAuthenticated() == true) {
+      return (
+        <button className="btn btn-primary" onClick={() => {
+          axios
+            .get("http://149.156.146.249:60021/api/logout")
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err));
+          auth.logout();
+          Cookies.remove('userName');
+          window.location.reload();
+        }}>Logout</button>)
     }
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid" id="navbarText">
+          <div className="navbar-header col-2">
+            <Link className="navbar-brand" to="">
+              <img className="col-12 pl-0" src={require('./logo_biale.png')} alt="" />
+            </Link>
+          </div>
+          <div className="navbar-nav">
+
+            <Link className="nav-link ml-5" to="praca">Oferty pracy</Link>
+            <Link className="nav-link ml-5" to="about">O nas</Link>
+            <Link className="nav-link ml-5" to="userpanel">Moje konto</Link>
+            <div className="ml-3">
+              {this.logoutButton()}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 
